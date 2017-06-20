0390a1dc8441561e4b418366baa52a48ab4b5d19
Nathan Mayer <nm@gmail.com>
2017-06-20 16:19:26 -0700

    refactor code to dry up, add css and js for result display and DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -6,53 +6,43 @@ var prime;
 var multiple;
 var primes = [];
 
+var formReset = function() {
+  primes = [];
+  numbers = [];
+  prime = 2;
+  $(".result").hide();
+}
+
 var primeGenerator = function(iteration) {
-
-
   for (var n = 2; n <= iteration; n += 1) {
     numbers.push(n);
   }
-
   primes = numbers.slice();
-
   for (var i = 1; i < iteration; i += 1) {
-
     for (var index = 1; prime + (prime * index)<= iteration; index +=1) {
       multiple = (prime +(prime * index));
-console.log("current multiple" + multiple);
       var pos = primes.indexOf(multiple);
-      console.log("index position" + pos);
-
       if (pos > 0) {
          primes.splice(pos,1);
       }
        else {
-      console.log("value doesn't exist within array primes");
       }
-        console.log("prime after: " + prime);
-
     }
-      prime += 1;
-      console.log(primes);
-      console.log(prime);
-
+    prime += 1;
   }
-console.log("functioncomplete");
 }
-
-
 //front-end
 $(document).ready(function() {
   $("input#nValue").click(function() {
-    primes = [];
-    numbers = [];
+    formReset();
   });
   $(".nInput form").submit(function(event) {
-    primes = [];
-    numbers = [];
-    prime = 2;
+    formReset();
     event.preventDefault();
     iteration = parseInt($("input#nValue").val());
     primeGenerator(iteration);
+    $(".result").show();
+    $("#numbers").text(numbers);
+    $("#primes").text(primes);
   });
 });
